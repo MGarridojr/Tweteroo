65e6acea747be4dedd3545693b7a06253cc12713
Driven Education <marcio.garrido.jr@gmail.com>
2022-04-25 16:07:19 -0300

    implementing sign up function

--- index.js
+++ index.js
@@ -3,11 +3,43 @@ import cors from 'cors'
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
-const teste = "foi agora"
+const users = []
+const tweets = []
+const error = []
 
-app.get('/', (req, res)=>{
-    res.send(teste)
+function validateUser(data){
+    const { username, avatar } = data
+    if(!username){
+        error.push("Usuário Inválido")
+    }
+    if(!avatar){
+        error.push("Foto Inválida")
+    }
+}
+
+app.post('/sign-up', (req, res)=>{
+    const user = req.body
+    validateUser(user)
+    if(error.length == 0){
+        users.push(user)
+        res.sendStatus(201)
+    }
+    else{
+        error.splice(0, error.length)
+        res.sendStatus(400)
+    }
+})
+
+app.post('/tweets', (req, res)=>{
+    const tweet  = req.body
+    tweets.push(tweet)
+    res.send("OK")
+})
+
+app.get('/tweets', (req, res) =>{
+    res.send(tweets)
 })
 
 app.listen(5000);
